dd1488f5e9050a7efd507ffa906bfa707fbc4783
Kanishk Thapa <78685048+Kanishktwork@users.noreply.github.com>
2021-10-19 17:24:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Test from "kanishkthapa-comp-test1/src/comp/test";
+function App() {
+  return (
+    <div>
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
